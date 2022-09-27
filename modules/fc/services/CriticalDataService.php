908dcfdfb578e652f726c3e85d02157987393487
Stephen Callender <stephen@fostercommerce.com>
2022-09-27 13:30:22 -0400

    Improved gateway data from service

--- modules/fc/services/CriticalDataService.php
+++ modules/fc/services/CriticalDataService.php
@@ -7,54 +7,50 @@ use craft\base\Component;
 use craft\commerce\Plugin;
 use craft\services\Security;
 
-class CriticalDataService extends Component {
+class CriticalDataService extends Component
+{
 
 	/**
 	 * Get the countries enabled in Commerce
 	 *
 	 * @return array
 	 */
-    public function getStoreCountries(): array {
-        return Plugin::getInstance()->getStore()->getStore()->getCountriesList();
-    }
+	public function getStoreCountries(): array
+	{
+		return Plugin::getInstance()->getStore()->getStore()->getCountriesList();
+	}
 
 	/**
 	 * Get the country regions enabled in Commerce
 	 *
 	 * @return array
 	 */
-    public function getStoreRegions(): array {
-        return Plugin::getInstance()->getStore()->getStore()->getAdministrativeAreasListByCountryCode();
-    }
+	public function getStoreRegions(): array
+	{
+		return Plugin::getInstance()->getStore()->getStore()->getAdministrativeAreasListByCountryCode();
+	}
 
 	/**
 	 * Get the enabled payment gateways in Commerce
 	 *
 	 * @return array
 	 */
-    public function getStorePaymentGateways(): array {
-        $gateways = Plugin::getInstance()->gateways->getAllCustomerEnabledGateways();
-        $result   = [];
+	public function getStorePaymentGateways(): array
+	{
+		$gateways = Plugin::getInstance()->gateways->getAllCustomerEnabledGateways();
 
-        foreach ($gateways as $gateway) {
-            $newGateway = [];
+		$gatewayData = [];
+		foreach ($gateways as $gateway) {
+			$g = [];
+			$g['handle'] = $gateway->handle;
+			$g['id'] = $gateway->id;
+			$g['name'] = $gateway->name;
+			$g['redirect'] = Craft::$app->getSecurity()->hashData('/order?number={number}');
+			$g['cancelUrl'] = Craft::$app->getSecurity()->hashData('/checkout');
+			$gatewayData[] = $g;
 
-            foreach ($gateway as $k => $v) {
-                if (preg_match('/^\$[A-Z0-9_]/', $v)) {
-                    $v = getenv(str_replace('$', '', $v));
-                }
+		}
 
-                if ($k === 'handle' && $v === 'paypal') {
-                    $newGateway['redirect']  = Craft::$app->getSecurity()->hashData('/order?number={number}');
-                    $newGateway['cancelUrl'] = Craft::$app->getSecurity()->hashData('/checkout');
-                }
-
-                $newGateway[$k] = $v;
-            }
-
-            $result[] = $newGateway;
-        }
-
-    	return $result;
+		return $gatewayData;
 	}
 }
