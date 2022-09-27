0ea60c87f62e30d35aed80a69e82c2a488251280
Rhys Matthew <77306946+rhysmatthew@users.noreply.github.com>
2022-09-27 15:40:02 -0400

    error page handling

---
+++ src/components/views/Pages404.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'Pages404'
+};
+</script>
+
+<template>
+  <div>
+    <!-- Page Header -->
+    <header class="bg-white">
+      <div class="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div class="text-center">
+          <p class="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">Error, page not found.</p>
+          <p class="max-w-xl mt-5 mx-auto text-xl text-gray-500"></p>
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
